4f585811dde1f0130ea9e2a4515fa8aef4e071d9
AISHWARYA MOURYA <0712aishwarya@gmail.com>
2024-05-09 12:26:58 +0530

    frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p> Our menu boasts a delightful selection of mouthwatering delights, meticulously crafted to satisfy every craving. From savory classics to innovative delights, we offer affordability without compromising on flavor or quality.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
