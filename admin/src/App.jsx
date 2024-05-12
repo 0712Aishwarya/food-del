698ff344d0a155dbac2b2cd9ad39b5d0659eaccd
AISHWARYA MOURYA <0712aishwarya@gmail.com>
2024-05-12 15:43:43 +0530

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import {Routes,Route} from 'react-router-dom'
+import Sidebar from './components/Sidebar/Sidebar'
+import Add from './pages/Add/Add'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+
+        </Routes>
+      </div>
     </div>
   )
 }
