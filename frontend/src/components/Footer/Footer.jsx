77d4b9e0f2ee8ec8da4a0bea7b273faa44e81698
AISHWARYA MOURYA <0712aishwarya@gmail.com>
2024-05-25 11:46:58 +0530

    changes

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
                     <img src={assets.twitter_icon} alt="" />
-                   <img  src={assets.linkedin_icon} alt="" />
+                   <a href="https://www.linkedin.com/in/aishwarya-mourya/"><img  src={assets.linkedin_icon} alt="" /></a>
                 </div>
             </div>
             <div className="footer-content-center">
